02b834d41f01d5998f44dc56e55ed6e3032771a9
Abrar Shekh <abrarshekh822@gmail.com>
2026-01-24 23:43:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,14 @@ export default function App() {
     return arr ? JSON.parse(arr) : []
   })
   const [msg, setMsg] = useState('')
+  const [deleteMsg, setDeleteMsg] = useState(false)
+  const[deleteId, setDeleteId] = useState(null)
+
   let totalTask = tasks.length
   let totalComplete = tasks.filter(t => t.completed === true).length
 
   useEffect(() => {
-    if(msg !== '') {
+    if(msg !== '' && !deleteMsg) {
       const id = setTimeout(() => setMsg(''), 2000)
       return () => clearTimeout(id)
     }
@@ -29,16 +32,39 @@ export default function App() {
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
 
+  function handleDeleteConfirmation() {
+    setTask(tasks.filter(t => t.id !== deleteId))
+    setMsg('Task Deleted Successfully.')
+    setDeleteMsg(false)
+    setDeleteId(null)
+  }
+
+  function handleDeleteCancellation() {
+    setMsg('Task Deletion Cancelled.')
+    setDeleteMsg(false)
+    setDeleteId(null)
+  }
+
   return (
     <>
       {
-        !msg && totalTask > 0 &&
+        deleteMsg &&
+        <div className="deleteMsg">
+          <strong>Shall We Delete The Task ?</strong>
+          <div className="btns">
+            <button onClick={handleDeleteConfirmation} className="deletionBtns">Yes</button>
+            <button className="deletionBtns" onClick={handleDeleteCancellation}>No</button>
+          </div>
+        </div>
+      }
+      {
+        !msg && totalTask > 0 && 
         <div className="topContainer">
           <div className="stats">Total: {totalTask} <pre>    |    </pre> Completed: {totalComplete} <pre>    |    </pre> Pending: {totalTask - totalComplete}</div>
         </div>
       }
       {
-        msg && 
+        !deleteMsg && msg && 
         <div className="msg">
           <strong>{msg}</strong>
         </div>
@@ -47,7 +73,7 @@ export default function App() {
 
       <Search search={search} setSearch={setSearch} activeFilter={activeFilter} setActiveFilter={setActiveFilter}/>
 
-      <TaskList tasks={tasks} search={search} setTask={setTask} setMsg={setMsg} setInput={setInput} editId={editId} setEditId={setEditId} input={input} activeFilter={activeFilter}/>
+      <TaskList tasks={tasks} search={search} setTask={setTask} setMsg={setMsg} setInput={setInput} editId={editId} setEditId={setEditId} input={input} activeFilter={activeFilter} setDeleteMsg={setDeleteMsg} deleteMsg={deleteMsg} setDeleteId={setDeleteId}/>
     </>
   )
 }
