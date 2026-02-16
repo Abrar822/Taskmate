2c44cb6e6b8e5e9d5c8994e2d57d5ae411a97fb5
Abrar Shekh <abrarshekh822@gmail.com>
2026-02-16 22:17:01 +0530

    Enhance task structure with date and deadline
    Added date and deadline properties to tasks.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,56 +1,83 @@
-import './App.css'
-import { useEffect } from 'react'
+import "./App.css";
+import { useEffect } from "react";
 
-export default function Header({ input, setInput, setTask, tasks, setMsg, editId, setEditId }) {
-
+export default function Header({
+  input,
+  setInput,
+  setTask,
+  tasks,
+  setMsg,
+  editId,
+  setEditId,
+}) {
   useEffect(() => {
     function handleKeyDown(e) {
-      if(input.trim() !== '' && e.key === 'Enter') {
-        addTask()
+      if (input.trim() !== "" && e.key === "Enter") {
+        addTask();
       }
-      if(editId !== null && e.key === 'Escape') {
-        setInput('')
-        setMsg('Edit Cancelled.')
-        setEditId(null)
+      if (editId !== null && e.key === "Escape") {
+        setInput("");
+        setMsg("Edit Cancelled.");
+        setEditId(null);
       }
     }
 
-    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener("keydown", handleKeyDown);
 
-    return (() => {
-      window.removeEventListener('keydown', handleKeyDown)
-    })
-  }, [tasks, input, editId])
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [tasks, input, editId]);
 
   function addTask() {
-    if(input.trim() !== '') {
-      if(editId) {
-        setTask(tasks.map(t => (t.id === editId ? {...t, task: input} : t)))
-        setMsg('Task Edited.')
-        setInput('')
-        setEditId(null)
+    if (input.trim() !== "") {
+      if (editId) {
+        setTask(
+          tasks.map((t) => (t.id === editId ? { ...t, task: input, date: Date.now() } : t)),
+        );
+        setMsg("Task Edited.");
+        setInput("");
+        setEditId(null);
       } else {
-        setTask([...tasks, {
-          id: Date.now() + Math.random(),
-          task: input.trim(),
-          completed : false
-        }])
-        setMsg('New Task Added.')
-        setInput('')
+        setTask([
+          ...tasks,
+          {
+            id: Date.now() + Math.random(),
+            task: input.trim(),
+            completed: false,
+            date: Date.now(), 
+            deadline: null,
+            deadlineDraft: null
+          },
+        ]);
+        setMsg("New Task Added.");
+        setInput("");
       }
     }
   }
 
-  return ( 
+  return (
     <>
-      <div className="header">
-        <h1 className="title">Taskmate</h1>
-      </div>
       <div className="inputContainer">
-        <input type="text" placeholder='Add Your New To-Do' value={input} onChange={(e) => setInput(e.target.value)}/>
-        <button title={editId  && input.trim() !== ''? 'Edit Task' : 'Add Task'} className="addTask" onClick={addTask}><i className="fa-solid fa-plus"></i></button>
+        <input
+          type="text"
+          placeholder="Add Your New To-Do"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          title={editId ? "Edit Task" : "Add Task"}
+          className="addTask"
+          onClick={addTask}
+        >
+          {!editId ? (
+            <i className="fa-solid fa-plus"></i>
+          ) : (
+            <i className="fa-solid fa-pen-to-square"></i>
+          )}
+        </button>
       </div>
     </>
-  )
-
+  );
 }
+
